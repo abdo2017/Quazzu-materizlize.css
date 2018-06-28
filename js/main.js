e37fec1e7b00633eb679c8c645d535cf87738fe3
abdo essam <abdo.esam2020@yandex.com>
2018-06-28 08:54:51 +0200

    finished project Quazzu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+// init menu nav  for mobile
+    $(".button-collapse").sideNav();
+// init carousel testimonials
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+// init modal
+    $('.modal').modal();
+// init select list 
+    $('select').material_select();
+	
+
+});
